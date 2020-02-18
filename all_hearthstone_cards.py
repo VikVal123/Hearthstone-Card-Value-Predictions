8090e7f0f1049e5b0e294352002aa8e8b10302aa
Viktoras Valiokas <viktoras.valiokas@gmail.com>
2020-02-18 09:31:50 +0200

    Test#2

--- all_hearthstone_cards.py
+++ all_hearthstone_cards.py
@@ -1,3 +1,3 @@
 
 
-# To start this project firstly I need to extract all of the existing Hearthstone cards and order them by format(Wild or Standart)
+# To start this project firstly I need to extract all of the existing Hearthstone cards and order them by format(Wild or Standart).
