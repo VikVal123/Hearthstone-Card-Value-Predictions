67ead3490f4d285ee88160b71625340df3da3fd7
Viktoras Valiokas <Viktoras.Valiokas@intermedix.com>
2020-02-25 20:36:29 +0200

    Update #1 Normalized and called out specific columns

--- cards_json_utf8.py
+++ cards_json_utf8.py
@@ -4,10 +4,12 @@ from pandas.io.json import json_normalize
 
 #cards.json file is encoded in UTF-8. 
 
-#pandas.set_option('display.max_rows', None)
-#pandas.set_option('display.max_columns', 6)
+# pandas.set_option('display.max_rows', 5)
+# pandas.set_option('display.max_columns', 5)
 
 with open('cards.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
+    raw_data = json.load(f)
+    data = pandas.json_normalize(raw_data)
 
-print(json_normalize(data))
+print(data[['id', 'name', 'cost', 'text', 'attack', 'health', 
+'cardClass', 'mechanics', 'rarity', 'set', 'type']])
